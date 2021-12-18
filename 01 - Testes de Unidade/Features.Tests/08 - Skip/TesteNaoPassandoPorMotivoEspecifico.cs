66d26dc8f276b21eb376f80380442c4f44ac9413
Evelym Rondon <evionline@gmail.com>
2021-12-18 15:27:22 -0400

    Escapendo de testes

---
+++ 01 - Testes de Unidade/Features.Tests/08 - Skip/TesteNaoPassandoPorMotivoEspecifico.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Features.Tests
+{
+    public class TesteNaoPassandoPorMotivoEspecifico
+    {
+        [Fact(DisplayName = "Novo Cliente 2.0", Skip = "Nova Versão 2.0 Quebrando")]
+        [Trait("Categoria", "Escapando dos Testes")]
+        public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
+        {
+            Assert.True(false);
+        }
+    }
+}
